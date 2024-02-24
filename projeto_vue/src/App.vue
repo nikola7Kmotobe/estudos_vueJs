c2dfdfd4fac36a0b58f0430d2062522c811acb10
nikola7Kmotobe <nikolamotobe@gmail.com>
2024-02-24 14:24:44 -0300

    Dados em componentes (data)

--- projeto_vue/src/App.vue
+++ projeto_vue/src/App.vue
@@ -1,9 +1,13 @@
+import
 <!--Com <script setup>, os componentes importados são disponibilizados automaticamente para o modelo.-->
 <script setup>
 import primeiro_componente  from './components/primeiro_componente.vue' // importando o compontente filho
+import segundo_componente from './components/segundo _componente.vue'
 </script>
 
 <template>
-  <h1>Here is a child component!</h1>
+  <h1>Primeiro Componente Filho</h1>
   <primeiro_componente />
+  <h1>Segundo Componente Filho</h1>
+  <segundo_componente />
 </template>
---
+++ projeto_vue/src/components/segundo _componente.vue
@@ -0,0 +1,14 @@
+<template>
+    <p>Meu nome é {{nome}} e minha ideade é {{idade}}</p>
+</template>
+
+<script>
+    export default{
+        data(){
+            return{
+                nome:"Kadu",
+                idade:26
+            }
+        }
+    }
+</script>
