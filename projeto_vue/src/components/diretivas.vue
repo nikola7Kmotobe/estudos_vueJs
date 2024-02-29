11636411dd9a2cf806b0a0dac177791f51ade9eb
nikola7Kmotobe <nikolamotobe@gmail.com>
2024-02-28 22:58:12 -0300

    Diretivas_2

--- projeto_vue/src/components/diretivas.vue
+++ projeto_vue/src/components/diretivas.vue
@@ -1,8 +1,17 @@
 <template>
     <div>
-        <h1>Testanto diretivas!</h1>
-        <p v-if="Verdadeiro">Vou aparecer pois mei v-if é verdadeiro</p>
+        <h1>Testanto diretivas! v-if</h1>
+        <p v-if="Verdadeiro">Vou aparecer pois mee v-if é verdadeiro</p>
         <p v-else>eu não pois sou falso!</p>
+    </div>
+    <div>
+        <h1>Testanto diretivas! v-bind</h1>
+        <img :src="src" :alt="alt" width="50" height="50">
+    </div>
+    <div>
+        <h1>Testanto diretivas!  @click -> metodos</h1>
+        <p>{{texto}}</p>
+        <button @click="CliqueAqui">Clique em mim</button>
     </div>
 </template>
 
@@ -11,8 +20,16 @@
         name:'diretivas',
         data(){
             return{
-                Verdadeiro: true
+                Verdadeiro: true,
+                src: "src/assets/nicotina.webp",
+                alt: "Meu Pigas!",
+                texto: "Led Zeppelin"
             }    
+        },
+        methods:{
+            CliqueAqui(){
+                this.texto = "Ser Constante e não ser Errante!"
+            }
         }
     }
 </script>
