b56e76697d46630c3a9c9702c31493c1424c4433
nikola7Kmotobe <nikolamotobe@gmail.com>
2024-02-28 22:31:04 -0300

    Diretivas

--- projeto_vue/src/App.vue
+++ projeto_vue/src/App.vue
@@ -1,10 +1,10 @@
-import
 <!--Com <script setup>, os componentes importados são disponibilizados automaticamente para o modelo.-->
 <script setup>
 import primeiro_componente  from './components/primeiro_componente.vue' // importando o compontente filho
 import segundo_componente from './components/segundo _componente.vue'
 import lifecycle_hooks from './components/lifecycle_hooks.vue';
 import Pessoa from './components/Pessoa.vue';
+import diretivas from './components/diretivas.vue';
 </script>
 
 <template>
@@ -16,4 +16,6 @@ import Pessoa from './components/Pessoa.vue';
   <lifecycle_hooks />
   <h1>Quarto Componente Filho</h1>
   <Pessoa/>
+  <h1>Quinto Componente Filho</h1>
+  <diretivas/>
 </template>
---
+++ projeto_vue/src/components/diretivas.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Testanto diretivas!</h1>
+        <p v-if="Verdadeiro">Vou aparecer pois mei v-if é verdadeiro</p>
+        <p v-else>eu não pois sou falso!</p>
+    </div>
+</template>
+
+<script>
+    export default{
+        name:'diretivas',
+        data(){
+            return{
+                Verdadeiro: true
+            }    
+        }
+    }
+</script>
